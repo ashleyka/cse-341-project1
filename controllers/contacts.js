c5c833454d2b6a770cc0e03585b32f7f53b30e8d
ashleyka <ashleykamujoma@gmail.com>
2025-03-25 16:30:08 +0200

    added 1

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,7 +10,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
+    const contactsId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactsId });
     result.toArray().then((contacts) => {
             res.setHeader('Content-Type', 'application/json');
